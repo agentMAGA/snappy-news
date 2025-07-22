6b7c26252a1d432219c332c5dd98cc3863b79473
agentMAGA <magaggntu@gmail.com>
2025-07-22 17:52:14 +0300

    Верстка домашней страницы

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+
+        <header className="header">
+
+          <div className="headerLogo">
+            <img src="/img/logo.svg"></img>
+          </div>
+
+          <div className='headerContent'>
+
+            <div className="headerSearch">
+
+              <div className='headerSearchInput'>
+                <input placeholder="Поиск"></input>
+              </div>
+
+              <div className='headerInputImg'>
+                <img src='/img/search.svg'></img>
+              </div>
+
+            </div>
+
+            <div className="categories">
+              <img src='/img/categories.svg'></img>
+              <p>Категории</p>
+            </div>
+
+            <div className='favorites'>
+              <img src='/img/favorites.svg'></img>
+              <p>Избранные</p>
+            </div>
+
+          </div>
+
+        </header>
+
+
+        <div className='news'>
+          <h6>НОВОСТИ</h6>
+        </div>
+
+        <div className='contentNews'>
+          
+        </div>
+
+
+      </div>
+
     </div>
   );
+
 }
 
 export default App;
