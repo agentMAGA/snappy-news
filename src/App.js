5619605bdd421d96e6d65e332093e70d80d9c5d4
agentMAGA <magaggntu@gmail.com>
2025-07-24 09:22:56 +0300

    Продолжение верстки

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import axios from "axios";
 
 
 function App() {
+
+  const [news, setNews] = React.useState([]);
+
+  React.useEffect(() => {
+
+    axios.get("https://687779a8dba809d901ef8e66.mockapi.io/test/items")
+    .then((res) => {
+      setNews(res.data);
+    })
+
+  }, []);
 
   return (
     <div className="App">
@@ -13,19 +25,21 @@ function App() {
             <img src="/img/logo.svg"></img>
           </div>
 
-          <div className='headerContent'>
 
-            <div className="headerSearch">
 
-              <div className='headerSearchInput'>
-                <input placeholder="Поиск"></input>
-              </div>
+          <div className="headerSearch">
 
-              <div className='headerInputImg'>
-                <img src='/img/search.svg'></img>
-              </div>
-
+            <div className='headerSearchInput'>
+              <input placeholder="Поиск"></input>
             </div>
+
+            <div className='headerInputImg'>
+              <img src='/img/search.svg'></img>
+            </div>
+
+          </div>
+
+          <div className='headerButton'>
 
             <div className="categories">
               <img src='/img/categories.svg'></img>
@@ -36,18 +50,115 @@ function App() {
               <img src='/img/favorites.svg'></img>
               <p>Избранные</p>
             </div>
-
+            
           </div>
+
+
 
         </header>
 
 
-        <div className='news'>
+        <div className='heading'>
           <h6>НОВОСТИ</h6>
         </div>
 
-        <div className='contentNews'>
-          
+        <div className='news'>
+
+          <div className='newsHeading'>
+            <h6>ПОПУЛЯРНЫЕ <b>НОВОСТИ</b></h6>
+          </div>
+
+          <div className='newsContent'>
+
+            <div className='newsCard'>
+
+              <div className='newsCardLeft'>
+
+                <div className='newsCardText'>
+                  <h6>Новости IT</h6>
+                  <p>Torem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus.</p>
+                </div>
+
+                <div className='newsCardButton'>
+
+                  <div className='button'>
+                    <button>Подробнее</button>
+                  </div>
+
+                  <div className='like'>
+                    <img src='./img/like.svg'></img>
+                  </div>
+
+                </div>
+
+              </div>
+
+              <div className='newsCardRight'>
+
+              </div>
+
+            </div>
+
+            <div className='newsCard'>
+
+              <div className='newsCardLeft'>
+
+                <div className='newsCardText'>
+                  <h6>Новости IT</h6>
+                  <p>Torem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus.</p>
+                </div>
+
+                <div className='newsCardButton'>
+
+                  <div className='button'>
+                    <button>Подробнее</button>
+                  </div>
+
+                  <div className='like'>
+                    <img src='./img/like.svg'></img>
+                  </div>
+
+                </div>
+
+              </div>
+
+              <div className='newsCardRight'>
+
+              </div>
+
+            </div>
+
+            <div className='newsCard'>
+
+              <div className='newsCardLeft'>
+
+                <div className='newsCardText'>
+                  <h6>Новости IT</h6>
+                  <p>Torem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus.</p>
+                </div>
+
+                <div className='newsCardButton'>
+
+                  <div className='button'>
+                    <button>Подробнее</button>
+                  </div>
+
+                  <div className='like'>
+                    <img src='./img/like.svg'></img>
+                  </div>
+
+                </div>
+
+              </div>
+
+              <div className='newsCardRight'>
+
+              </div>
+
+            </div>
+
+          </div>
+
         </div>
 
 
